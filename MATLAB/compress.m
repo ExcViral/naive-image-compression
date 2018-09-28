b33523d3fbe4724adf480e24eed0b8f6d6f579af
Viral Patel <exc.viral@gmail.com>
2018-09-28 22:07:30 +0530

    Implemented compression algorithm and image reconstruction algorithm

--- MATLAB/compress.m
+++ MATLAB/compress.m
@@ -1,13 +1,21 @@
 clc;
 clear all;
 
-% fill in the image name here
-I = imread('imageName.imageFormat');
-imshow(I);
+% input image
+I = imread('');
+figure("Name","Input Image"); imshow(I);
 
-% SVD decomposition of image
+% SVD decomposition of input image
 [U,S,V] = svd(double(I));
 
-% algorithm of image comression
+% algorithm of image compression
+img_size = size(I);
+new_S = S;
+compression_level = 200;
+for k=0:compression_level
+   new_S(img_size(1)-k,img_size(2)-k) = 0;
+end
 
-
+% reconstruction of image
+new_I = uint8(U*new_S*V');
+figure("Name","Compressed Image"); imshow(new_I);
